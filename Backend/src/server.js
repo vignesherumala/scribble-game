118dc3495f1d0a6464237462303b87ce79ea6861
Vignesh Erumala <vignesherumala@gmail.com>
2025-12-21 23:44:01 +0530

    Prepare backend for deployment

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -8,14 +8,15 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
 
+
 app.use(cors());
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 /* ---------------- GAME STATE ---------------- */
 
