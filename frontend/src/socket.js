6b7dce4f7fed89bff8223eeb604fc4ba63275c42
Vignesh Erumala <vignesherumala@gmail.com>
2025-12-22 00:09:47 +0530

    Update socket URL for production

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:5000", {
+export const socket = io("https://scribble-game-bjrn.onrender.com", {
   transports: ["websocket"],
   autoConnect: true
 });
